218505631ea676b84f8a7f737d2caeaee5eb135c
zhaolai1102 <zhaolai1102@163.com>
2019-09-06 15:51:40 +0800

    123

--- zhaolai_cost_m.js
+++ zhaolai_cost_m.js
@@ -26,16 +26,4 @@ $(function(){
 		mySwiper.destroy();
 		make_lunbo();
 	});
-	$('.zd-btn').click(function(){
-		let target = $(event.target);
-		if(target.text()==='展开'){
-			target.empty();
-			target.append('<img src="./assets/shousuo1@2x.png" class="zd-pic2">收缩');
-		}else{
-			target.empty();			
-			target.append('<img src="./assets/zhankai1@2x.png" class="zd-pic2">展开');
-		}
-		let show = $('#'+target.attr('show'));
-		show.slideToggle();
-	})
 });
